200698f11aae80eafd228e23bef810cd2a48bc65
spaceman-haley <spaceman@Haleys-MacBook-Pro.local>
2017-05-09 15:30:31 -0500

    DB update
    updated db to include all needed tables and finalize structure

--- db/ipad.sql
+++ db/ipad.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 09, 2017 at 12:55 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Host: localhost
+-- Generation Time: May 09, 2017 at 10:29 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,15 +31,17 @@ CREATE TABLE `chapman` (
   `ID` int(11) NOT NULL,
   `serial` int(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `funding` varchar(255) NOT NULL
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `chapman`
 --
 
-INSERT INTO `chapman` (`ID`, `serial`, `description`, `funding`) VALUES
-(1, 132, 'iPad mini', 'Title 1');
+INSERT INTO `chapman` (`ID`, `serial`, `description`, `funding`, `location`, `found`) VALUES
+(1, 132, 'iPad mini', 'Title 1', '', 0);
 
 -- --------------------------------------------------------
 
@@ -52,15 +54,108 @@ CREATE TABLE `dawson` (
   `ID` int(11) NOT NULL,
   `serial` int(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `funding` varchar(255) NOT NULL
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `dawson`
 --
 
-INSERT INTO `dawson` (`ID`, `serial`, `description`, `funding`) VALUES
-(1, 111, 'iPad Air', 'SIG');
+INSERT INTO `dawson` (`ID`, `serial`, `description`, `funding`, `location`, `found`) VALUES
+(1, 111, 'iPad Air', 'SIG', '', 'true'),
+(2, 122, 'iPad again', 'SIG', '23', 'true'),
+(3, 333, 'iPad test', 'Title 1', '23', 'true'),
+(4, 555, 'Ipad', 'SIG', '43', 'true'),
+(5, 6667, 'ipad', '', '', 'true');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `highlands`
+--
+
+DROP TABLE IF EXISTS `highlands`;
+CREATE TABLE `highlands` (
+  `ID` int(11) NOT NULL,
+  `serial` int(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lakewood`
+--
+
+DROP TABLE IF EXISTS `lakewood`;
+CREATE TABLE `lakewood` (
+  `ID` int(11) NOT NULL,
+  `serial` int(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mcdonnell`
+--
+
+DROP TABLE IF EXISTS `mcdonnell`;
+CREATE TABLE `mcdonnell` (
+  `ID` int(11) NOT NULL,
+  `serial` int(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mlk`
+--
+
+DROP TABLE IF EXISTS `mlk`;
+CREATE TABLE `mlk` (
+  `ID` int(11) NOT NULL,
+  `serial` int(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `montview`
+--
+
+DROP TABLE IF EXISTS `montview`;
+CREATE TABLE `montview` (
+  `ID` int(11) NOT NULL,
+  `serial` int(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `montview`
+--
+
+INSERT INTO `montview` (`ID`, `serial`, `description`, `funding`, `location`, `found`) VALUES
+(1, 132, 'iPad mini', 'Title 1', '', 0);
 
 -- --------------------------------------------------------
 
@@ -73,16 +168,73 @@ CREATE TABLE `morris` (
   `ID` int(11) NOT NULL,
   `serial` int(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `funding` varchar(255) NOT NULL
+  `funding` varchar(255) NOT NULL,
+  `found` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `morris`
+-- Table structure for table `ridgecrest`
 --
 
-INSERT INTO `morris` (`ID`, `serial`, `description`, `funding`) VALUES
-(1, 1234, 'iPad 4', 'Title 1'),
-(2, 12345, 'Ipad 2', 'SIG');
+DROP TABLE IF EXISTS `ridgecrest`;
+CREATE TABLE `ridgecrest` (
+  `ID` int(11) NOT NULL,
+  `serial` int(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rollinghills`
+--
+
+DROP TABLE IF EXISTS `rollinghills`;
+CREATE TABLE `rollinghills` (
+  `ID` int(11) NOT NULL,
+  `serial` int(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shereford`
+--
+
+DROP TABLE IF EXISTS `shereford`;
+CREATE TABLE `shereford` (
+  `ID` int(11) NOT NULL,
+  `serial` int(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `westlawn`
+--
+
+DROP TABLE IF EXISTS `westlawn`;
+CREATE TABLE `westlawn` (
+  `ID` int(11) NOT NULL,
+  `serial` int(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `funding` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `found` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -101,9 +253,63 @@ ALTER TABLE `dawson`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `highlands`
+--
+ALTER TABLE `highlands`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `lakewood`
+--
+ALTER TABLE `lakewood`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `mcdonnell`
+--
+ALTER TABLE `mcdonnell`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `mlk`
+--
+ALTER TABLE `mlk`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `montview`
+--
+ALTER TABLE `montview`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `morris`
 --
 ALTER TABLE `morris`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `ridgecrest`
+--
+ALTER TABLE `ridgecrest`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `rollinghills`
+--
+ALTER TABLE `rollinghills`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `shereford`
+--
+ALTER TABLE `shereford`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `westlawn`
+--
+ALTER TABLE `westlawn`
   ADD PRIMARY KEY (`ID`);
 
 --
@@ -119,12 +325,57 @@ ALTER TABLE `chapman`
 -- AUTO_INCREMENT for table `dawson`
 --
 ALTER TABLE `dawson`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `highlands`
+--
+ALTER TABLE `highlands`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `lakewood`
+--
+ALTER TABLE `lakewood`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `mcdonnell`
+--
+ALTER TABLE `mcdonnell`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `mlk`
+--
+ALTER TABLE `mlk`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `montview`
+--
+ALTER TABLE `montview`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `morris`
 --
 ALTER TABLE `morris`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ridgecrest`
+--
+ALTER TABLE `ridgecrest`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `rollinghills`
+--
+ALTER TABLE `rollinghills`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `shereford`
+--
+ALTER TABLE `shereford`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `westlawn`
+--
+ALTER TABLE `westlawn`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
