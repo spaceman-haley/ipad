88afe8845788f1b542668663557219ec6d5bfead
spaceman-haley <dr.english10@gmail.com>
2017-05-18 21:46:02 -0500

    Add files via upload

--- highlandsform.php
+++ highlandsform.php
@@ -3,8 +3,12 @@
 	$_SESSION['$school'] = "highlands";
 	$_SESSION['$url'] = "highlandsform.php";
 	include 'includes/header.php';
+	include 'includes/functions.php';
+	include_once 'includes/dbconn.php';
 ?>
 
+<?php if (login_check($mysqli) == true) : ?>
+     <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>
 <center><h2>Highlands Elementary</h2></center>
 
 <form action="highlandsform.php" method = "POST">
@@ -25,5 +29,11 @@
 	include 'includes/report.php';
 
 ?>
+
+<?php else : ?>
+            <p>
+                <span class="error">You are not authorized to access this page.</span> Please <a href="index.php">login</a>.
+            </p>
+        <?php endif; ?>
 	</body>
 </html>
